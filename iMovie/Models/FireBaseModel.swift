0ada106251beae919e427c4672d8e61c4527d40e
ronavraham <roniii3207@cs.colman.ac.il>
2019-01-26 12:45:01 +0200

    create the basics of the main story board, including firebase communication. main story board welcome page now supports register & login trough firebase.

---
+++ iMovie/Models/FireBaseModel.swift
@@ -0,0 +1,108 @@
+//
+//  FireBaseModel.swift
+//  iMovie
+//
+//  Created by admin on 26/01/2019.
+//  Copyright © 2019 admin. All rights reserved.
+//
+
+import Foundation
+import Firebase
+import FirebaseDatabase
+import FirebaseStorage
+
+class FireBaseModel {
+    var ref:DatabaseReference?
+    var storageRef:StorageReference?
+    
+    static var instance: FireBaseModel = FireBaseModel()
+    
+    static func getInstance()->FireBaseModel {
+        return instance
+    }
+    
+    private init() {
+        ref = nil
+        storageRef = nil
+    }
+    
+    func initRefs() {
+        ref = Database.database().reference()
+        storageRef = Storage.storage().reference();
+    }
+    
+    func registerUser(email:String, password:String, callback:@escaping (AuthDataResult?)->Void) {
+        Auth.auth().createUser(withEmail: email, password: password, completion: { (user, error) in
+            if error != nil {
+                callback(nil)
+            } else {
+                callback(user)
+            }
+        })
+    }
+    
+    func signIn(email:String, password:String, callback:@escaping (AuthDataResult?)->Void) {
+        Auth.auth().signIn(withEmail: email, password: password) { (user, error) in
+            if error != nil {
+                callback(nil)
+            } else {
+                callback(user)
+            }
+        }
+    }
+    
+    func connectedUser() -> User? {
+        return Auth.auth().currentUser;
+    }
+    
+    func getAutoKey(table:String)->String? {
+        return self.ref?.child(table).childByAutoId().key
+    }
+    
+    func getAllItemsInTable(table:String, callback:@escaping ([String:[String:Any]]?)->Void) {
+        self.ref?.child(table).observeSingleEvent(of: .value, with: { (snapshot) in
+            callback(snapshot.value as? [String:[String:Any]])
+        })
+    }
+    
+    func addItemToTable(table:String, key:String, value:[String:Any]) {
+        self.ref?.child(table).child(key).setValue(value)
+    }
+    
+    func removeItemFromTable(table:String, key:String) {
+        self.ref?.child(table).child(key).removeValue()
+    }
+    
+    func saveImageToFirebase(image:UIImage, name:(String),
+                             callback:@escaping (String?)->Void){
+        let filesRef = storageRef!.child("images/"+name+".jpg")
+        if let data = image.jpegData(compressionQuality: 0.8) {
+            filesRef.putData(data, metadata: nil) { metadata, error in
+                if (error != nil) {
+                    callback(nil)
+                } else {
+                   self.storageRef?.downloadURL { url, error in callback(url?.absoluteString) }
+                }
+            }
+        }
+    }
+    
+    func downloadImage(name:String, callback:@escaping (UIImage?)->Void) {
+        let islandRef = storageRef!.child("images/"+name+".jpg")
+        islandRef.getData(maxSize: 5 * 1024 * 1024) { data, error in
+            if (error != nil) {
+                callback(nil)
+            } else {
+                
+                let image = UIImage(data: data!)
+                callback(image)
+            }
+        }
+    }
+    
+    func removeImage(name:String) {
+        storageRef!.child("images/"+name+".jpg").delete { (error) in
+        }
+    }
+}
+
--- iMovie/ViewController.swift
+++ iMovie/ViewController.swift
@@ -10,17 +10,78 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    let model: FireBaseModel = FireBaseModel.getInstance()
+    
+    @IBOutlet weak var Spinner: UIActivityIndicatorView!
     @IBOutlet weak var EmailField: UITextField!
     @IBOutlet weak var PasswordField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        self.Spinner.stopAnimating()
+        self.Spinner.isHidden = true
     }
 
     @IBAction func login(_ sender: Any) {
+        let email = self.EmailField.text
+        let password = self.PasswordField.text
+        if email != "" {
+            if password != "" {
+                self.Spinner.isHidden = false
+                self.Spinner.startAnimating()
+                model.signIn(email: email!, password: password!, callback: { (user) in
+                    self.Spinner.stopAnimating()
+                    self.Spinner.isHidden = true
+                    if user != nil {
+                        self.model.initRefs()
+                        self.performSegue(withIdentifier: "loginToApp", sender: self)
+                    } else {
+                        self.openMessageBox(title: "Failed", message: "login failed")
+                    }
+                })
+            } else {
+                self.openMessageBox(title: "Empty password", message: "password can't be empty")
+            }
+        } else {
+            self.openMessageBox(title: "Empty email", message: "email can't be empty")
+        }
     }
     
     @IBAction func register(_ sender: Any) {
+        let email = self.EmailField.text
+        let password = self.PasswordField.text
+        if email != "" {
+            if password != "" {
+                if password!.count < 6 {
+                    self.openMessageBox(title: "Password too short", message: "has to be more than 6 characters")
+                } else {
+                    self.Spinner.isHidden = false
+                    self.Spinner.startAnimating()
+                    model.registerUser(email: email!, password: password!, callback: { (user) in
+                        self.Spinner.stopAnimating()
+                        self.Spinner.isHidden = true
+                        if user != nil {
+                            self.model.initRefs()
+                            self.performSegue(withIdentifier: "loginToApp", sender: self)
+                        } else {
+                            self.openMessageBox(title: "Failed", message: "registaration failed")
+                        }
+                    })
+                }
+            } else {
+                self.openMessageBox(title: "Empty password", message: "password can't be empty")
+            }
+        } else {
+            self.openMessageBox(title: "Empty email", message: "email can't be empty")
+        }
+    }
+    
+    func openMessageBox(title: String, message: String) {
+        let alert = UIAlertController(title: title,
+                                      message: message,
+                                      preferredStyle: UIAlertController.Style.alert)
+        alert.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil))
+        self.present(alert, animated: true, completion: nil)
     }
 }
 
